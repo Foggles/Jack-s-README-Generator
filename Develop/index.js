9a120110e4cbaf3a9997c377b4d9939deea9d639
Jack Fogarty <jackfogarty2105@gmail.com>
2021-01-21 17:35:48 +0800

    Built questions that the user will be asked

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the name of your project?',
+            name: 'projectTitle',
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        {
+            type: 'input',
+            message: 'What is the description of your project?',
+            name: 'projectDescription',
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type: 'input',
+            message: 'What are the installation instructions for your project?',
+            name: 'projectInstallation',
+        },
 
-// Function call to initialize app
-init();
+        {
+            type: 'input',
+            message: 'What is the usage information for your project?',
+            name: 'projectUsage',
+        },
+
+        {
+            type: 'input',
+            message: 'How could someone contribute to your project?',
+            name: 'projectContribution',
+        },
+
+        {
+            type: 'input',
+            message: 'How should someone test your project?',
+            name: 'projectTesting',
+        },
+
+        {
+            type: 'list',
+            message: 'What license will your project be under?',
+            name: 'projectLicense',
+            choices: ['1', '2', '3', '4'],
+        },
+
+        {
+            type: 'input',
+            message: 'What is your GitHub username? (case sensitive)?',
+            name: 'gitHubUsername',
+        },
+
+        {
+            type: 'input',
+            message: 'What is your email?',
+            name: 'userEmail',
+        }
+    ]);
